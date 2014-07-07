f8a2cc41a27efeb08bb1f51687e9c9b5f2fe0bee
CL-Shang <shang@shangdeMacBook-Pro.local>
2014-07-07 15:40:09 +0800

    0.1.2
    修改识别规则

--- my-plugins/kesioncms.rb
+++ my-plugins/kesioncms.rb
@@ -5,8 +5,8 @@
 # http://www.morningstarsecurity.com/research/whatweb
 ##
 Plugin.define "kesioncms" do
-author "shang <s@suu.cc>" # 2014-06-30
-version "0.1.1"
+author "shang <s@suu.cc>" # 2014-07-07
+version "0.1.2"
 description "KesionCMS [Chinese] - Homepage: http://www.kesion.com/"
 
 # Examples #
@@ -24,7 +24,7 @@ matches [
   {:text=>"/ks_inc/kesion.common.js"},
   {:text=>"KesionJS"},
   {:text=>"KesionPopup"},
-  {:text=>"Powered By KesionICMS ",:version=>/V([\d\.]+)/m},
+  {:version=>/Powered By KesionICMS V([\d\.]+)/m},
   {:text=>"powered by <a href=\"http://www.kesion.com\" target=\"_blank\">",:version=>/KesionCMS ([\d\.]+)/m},
   {:url=>"/KS_Inc/ajax.js",:version=>/KesionCMS V([\d\.]+)/m},
 ]
