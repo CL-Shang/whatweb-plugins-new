049c9429a07c1c50728fb464e6886d048aad2168
CL-Shang <shang@shangdeMacBook-Pro.local>
2014-07-07 15:48:27 +0800

    0.1.1
    修改了部分内容

--- my-plugins/foosuncms.rb
+++ my-plugins/foosuncms.rb
@@ -4,7 +4,7 @@
 # web site for more information on licensing and terms of use.
 # http://www.morningstarsecurity.com/research/whatweb
 ##
-Plugin.define "foosuncms" do
+Plugin.define "FoosunCMS" do
 author "shang <s@suu.cc>" # 2014-06-30
 version "0.1.1"
 description "FoosunCMS [Chinese] - Homepage: http://www.foosun.cn/"
