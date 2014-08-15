42e1a4a81f6be9104da4ea052cf5a7492375537d
CL-Shang <shang@shangdeMacBook-Pro.local>
2014-08-15 16:25:58 +0800

    0.1.2
    更新 2008，2007，V9的指纹

--- my-plugins/phpcms.rb
+++ my-plugins/phpcms.rb
@@ -5,8 +5,8 @@
 # http://www.morningstarsecurity.com/research/whatweb
 ##
 Plugin.define "phpCMS" do
-author "shang <s@suu.cc>" # 2014-07-02
-version "0.1.1"
+author "shang <s@suu.cc>" # 2014-08-15
+version "0.1.2"
 description "phpCMS [Chinese] - Homepage: http://www.phpcms.cn/"
 
 # Dorks #
@@ -18,10 +18,13 @@ dorks [
 matches [
 
   # url exists, i.e. returns HTTP status 200
-
-  {:text=>"phpcms"},
   {:text=>"<meta name=\"phpCMS.robots"},
   {:text=>"<!-- PHPCMS_NOINDEX"},
+  {:url=>"/statics/css/default_blue.css",:version=>/images\/v([\d\.]+)\/body-bg.png/},#php9
+  {:text=>"/statics/css/default_blue.css"},
+  {:version=>/blank\">Powered by Phpcms ([\d\.]+)</},#php 2008 
+  {:version=>/strong> <em>V([\d\.]+)</},#php 9.1.13,9.2.7
+  {:version=>/<meta name=\"generator\" content=\"Phpcms ([\d\.]+)\">/} #php 2007
   
 ]
 
